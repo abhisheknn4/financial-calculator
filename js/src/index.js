f927436de423e4a5c3639cae276878bd419e3fd9
Abhishek Borar <abhishek.borar@nn4energy.com>
2018-03-23 15:46:41 +0530

    basin emi calculator

--- js/src/index.js
+++ js/src/index.js
@@ -1,3 +1,5 @@
+require("./sliders.js");
+
 if('serviceWorker' in navigator) {
 	navigator.serviceWorker.register('/sw.js').catch(function(err) {console.err("Error registering service worker", err);});
 }
