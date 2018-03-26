eb67219e1a263231057d6781fa11a92e87f6faa0
Abhishek Borar <abhishek.borar@nn4energy.com>
2018-03-26 16:32:54 +0530

    adding variance graph and styling

--- js/src/sliders.js
+++ js/src/sliders.js
@@ -1,13 +1,14 @@
 var changeHandlerStateKey = "lastHandledValue";
 
-function getEmi(){
-	var principle = $('#input-principle').val();
-	console.log("principle: " + principle);
-	var interest = $('#input-interest-rate').val() / 1200;
-	console.log("interest: " + interest);
-	var months = $('#input-years').val() * 12;
-	console.log("years: " + $('#input-years').val());
-	console.log("months: " + months);
+function isset(val){
+	return typeof val != 'undefined';
+}
+
+function getEmi(params){
+	params = params || {};
+	var principle = isset(params.principle) ? params.principle : $('#input-principle').val();
+	var interest = (isset(params.interest)? params.interest : $('#input-interest-rate').val()) / 1200;
+	var months = (isset(params.years) ? params.years : $('#input-years').val()) * 12;
 	
 	var powerVal = Math.pow(1+interest, months);
 	return Math.round((principle * interest * powerVal)/(powerVal - 1)*100)/100;
@@ -19,6 +20,7 @@ function changeHandler(elem){
 		$('.coupled-input[data-id='+(elem.attr('data-id'))+']').attr(changeHandlerStateKey, elem.val());
 		var emi = getEmi();
 		$('#calculated-emi').text(emi);
+		drawGraphs();
 	}
 }
 
@@ -34,4 +36,75 @@ $(document).ready(function(){
 	$('input[type=range]').each(function(){
 		changeHandler($(this));
 	});
+});
+
+/***********Charts*************/
+
+function getDivs(params){
+	var totalColumns = params.data.length;
+	var width = 100/totalColumns;
+	
+	var maxValue = -Infinity;
+	params.data.forEach(function(o){
+		if(o.val > maxValue) maxValue = o.val;
+	});
+	var multiplier = 100/maxValue;
+	
+	return params.data.map(function(o){
+		return "<div style=\"width:"+width+"%;\" class=\"column\" range=\""+o.range+"\">\
+			<div style=\"height:"+(100 - (o.val*multiplier))+"%;\" class=\"column-value\" value=\""+o.val+"\"></div>\
+		</div>"
+	});
+}
+
+function getData(params){
+	var minValue = params.input.attr('min');
+	var maxValue = params.input.attr('max');
+	var range = [];
+	var sliderStep = parseFloat(params.input.attr('step'));
+	var stepSize = (maxValue-minValue)/params.numSteps;
+	for(var i=0; i<params.numSteps; i++){
+		var x = (i*stepSize) + (stepSize/2);
+		var multiplier = Math.round(x/sliderStep);
+		var roundX = sliderStep*multiplier;
+		
+		var variateBlock = {};
+		variateBlock[params.variateKey] = roundX;
+		
+		range.push({
+			range: roundX,
+			val: getEmi(variateBlock)
+		});
+	}
+	return range;
+}
+
+function drawGraphs(){
+	$('#variance-years').html(getDivs({
+		data: getData({
+			numSteps: 40,
+			input: $('#input-years'),
+			variateKey: 'years'
+		})
+	}));
+	
+	$('#variance-principle').html(getDivs({
+		data: getData({
+			numSteps: 40,
+			input: $('#input-principle'),
+			variateKey: 'principle'
+		})
+	}));
+	
+	$('#variance-interest-rate').html(getDivs({
+		data: getData({
+			numSteps: 40,
+			input: $('#input-interest-rate'),
+			variateKey: 'interest'
+		})
+	}));
+}
+
+$(document).ready(function(){
+	drawGraphs();
 });
